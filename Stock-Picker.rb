d4b0df2d376095ab0ac9617dd75a19fd32ef035e
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-03-28 23:19:12 +0200

    solve, work properly

--- Stock-Picker.rb
+++ Stock-Picker.rb
@@ -1,10 +1,10 @@
 def stock_picker(array)
-
   result = {}
- array.each_with_index do |operand, index|
-  array.each_with_index {|operand2, index2| result[operand2-operand] = [index, index2] if index < index2}
- end
-  result
-  
 
+  array.each_with_index do |operand, index|
+    array.each_with_index {|operand2, index2| result[operand2-operand] = [index, index2] if index < index2}
+  end
+
+  max_profit = result.keys.max
+  result[max_profit]
 end
