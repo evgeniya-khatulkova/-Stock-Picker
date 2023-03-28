6053b3e61fcb819719f4a81ba0722516c02a567f
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-03-28 14:40:30 +0200

    find day to buy for the best stock result

--- Stock-Picker.rb
+++ Stock-Picker.rb
@@ -4,5 +4,8 @@ def stock_picker(array)
  array.each_with_index do |operand, index|
     array.each_with_index {|operand2, index2| result << operand2 - operand if index < index2}
  end
-  p result
+  result
+  position = result.each_with_index.max[1]
+  first_day = position / (array.length - 1)
+
 end
