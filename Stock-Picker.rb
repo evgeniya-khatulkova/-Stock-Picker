74aa099aa1073747a810e3447e0ca84f9571cf2f
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-03-28 12:02:32 +0200

    fix substraction only with the days in future

--- Stock-Picker.rb
+++ Stock-Picker.rb
@@ -1,7 +1,8 @@
 def stock_picker(array)
+
   result = []
-  array.each_with_index do |operand, index|
-    array.each {|operand2| result << operand2 - operand}
-  end
-  result
+ array.each_with_index do |operand, index|
+    array.each_with_index {|operand2, index2| result << operand2 - operand if index < index2}
+ end
+  p result
 end
